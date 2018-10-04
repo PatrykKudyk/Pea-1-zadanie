31a87670a55121ea87cb6b75cb327b3d69be0ae5
Unknown <gustaw5022@gmail.com>
2018-10-04 17:01:28 +0200

    drobne poprawki w wyswietlaniu grafu żeby było śliczniej :)

--- Pea pierwsze zadanie/Pea pierwsze zadanie/Graph.cpp
+++ Pea pierwsze zadanie/Pea pierwsze zadanie/Graph.cpp
@@ -67,25 +67,26 @@ void Graph::display()
 			{
 				cout << "\t";
 				for (int m = 0; m < verticle; m++)
-					cout << m << "\t";
-				cout << endl;
-				for (int ilosc = 0; ilosc < verticle * 8; ilosc++)	//wypisanie wiersza skladajacego sie z ciagu: "----(...)--"
+					cout << m << "\t";	//wypisuje numery wierzcholkow w pierwszym wierszu
+				cout << "<--- numery wierzcholkow" << endl;
+				for (int ilosc = 0; ilosc < (verticle * 8) + 2; ilosc++)	//wypisanie wiersza skladajacego sie z ciagu: "----(...)--"
 					cout << "-";
 				cout << endl;
 			}
-			cout << i << "\t";
+			cout << i << "\t"; // wypisanie numeru wiersza w pierwszej kolumnie
 			for (int j = 0; j < verticle; j++)
 				cout << graph[i][j] << "\t";					//wypisywanie wartosci z macierzy
 
 			cout << endl;
 			if (i % 2 == 1)
-				for (int ilosc = 0; ilosc < verticle * 8; ilosc++)		//co drugi wiersz oddzielony jest ciagiem: "----(...)--"
+				for (int ilosc = 0; ilosc < (verticle * 8) + 2; ilosc++)		//co drugi wiersz oddzielony jest ciagiem: "----(...)--"
 					cout << "-";
 			else
-				for (int ilosc = 0; ilosc < verticle * 8; ilosc++)	//co drugi wiersz oddzielony jest ciagiem: "~~~~(...)~~"
+				for (int ilosc = 0; ilosc < (verticle * 8) + 2; ilosc++)	//co drugi wiersz oddzielony jest ciagiem: "~~~~(...)~~"
 					cout << "~";
 			cout << endl;
 		}
+		cout << "^\n|\n|\nwierzcholki" << endl;
 	}
 	else
 		cout << "Graf nie posiada wierzcholkow, wiec nie mozna go wyswietlic." << endl;
